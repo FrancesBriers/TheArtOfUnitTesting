ce8abcb3bb65d393f45cd83920166ed60161bb20
fbriers <fbriers@west.com>
2018-11-26 17:14:08 +0000

    Add project files.

---
+++ LogAn.UnitTests/MockingUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    public class EmailInfo
+    {
+        public string Body;
+        public string To;
+        public string Subject;
+    }
+    public class FakeWebService : IWebService
+    {
+        public string LastError;
+        public Exception ToThrow;
+        public void LogError(string message)
+        {
+            LastError = message;
+        }
+    }
+    public class FakeEmailService : IEmailService
+    {
+        public EmailInfo email = null;
+        public void SendEmail (EmailInfo emailInfo)
+        {
+            email = emailInfo;
+        }
+    }
+
+    [TestFixture]
+    class MockingUnitTests
+    {
+        [Test]
+        public void Analyze_TooShortFileName_CallsWebService()
+        {
+            FakeWebService mockService = new FakeWebService();
+            LogAnalyzer log = new LogAnalyzer(mockService);
+            string tooShortFileName = "abc.ext";
+
+            log.IsValidLogFileName(tooShortFileName);
+
+            StringAssert.Contains("Filename too short: abc.ext", mockService.LastError);
+
+        }
+
+    }
+
+    [TestFixture]
+    public class LogAnalyzer2Tests
+    {
+        //Tests the Stub web service and Mock email sender
+        [Test]
+        public void Analyze_WebServiceThrows_SendsEmail()
+        {
+            FakeWebService stubService = new FakeWebService();
+            stubService.ToThrow = new Exception("fake exception");
+
+            FakeEmailService mockEmail = new FakeEmailService();
+            LogAnalyzer2 log = new LogAnalyzer2(stubService, mockEmail);
+
+            string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+
+            EmailInfo expectedEmail = new EmailInfo
+            {
+                Body = "fake exception",
+                To = "someone@somewhere.com",
+                Subject = "can't log"
+            };
+            Assert.AreEqual(expectedEmail, mockEmail.email);
+        }
+    
+    }
+
+  
+    }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LogAn
+{
+    public interface IWebService
+    {
+        void LogError(string message);
+    }
+
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+
+    public class LogAnalyzer
+    {
+        private IWebService service;
+        public bool WasLastFileNameValid { get; set; }
+
+        public LogAnalyzer(IWebService service)
+        {
+            this.service = service;
+        }
+
+        public void Analyze(string fileName)
+        {
+            if (fileName.Length < 8)
+            {
+                service.LogError("Filename too short: " + fileName);
+            }
+        }
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("filename has to be provided");
+            }
+            if (!fileName.EndsWith(".SLF",StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            WasLastFileNameValid = true;
+            return true;
+        }
+
+    }
+
+    public class LogAnalyzer2
+    {
+        private IWebService service;
+        private IEmailService email;
+
+        public LogAnalyzer2(IWebService service, IEmailService email)
+        {
+            Email = email;
+            Service = service;
+        }
+        public IEmailService Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public IWebService Service
+        {
+            get { return service; }
+            set { service = value; }          
+        }
+        public void Analyze(string fileName)
+        {
+            if(fileName.Length<8)
+            {
+                try
+                {
+                    Service.LogError("Filename too short: " + fileName);
+                }
+                catch (Exception e)
+                {
+                    email.SendEmail("someone@somewhere.com", "cannot log", e.Message);
+                }
+            }
+        }
+    }
+}
